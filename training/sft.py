e9b9060f54c63bcd896979ef2eecfd79383dce7b
Matvey Saprykin <mtvey.s@gmail.com>
2024-09-07 00:04:36 +0300

    added data

--- training/sft.py
+++ training/sft.py
@@ -2,5 +2,5 @@ import subprocess
 from pathlib import Path
 
 if __name__ == '__main__':
-    launch_code = 'python -m turbo_alignment train_sft --experiment_settings_path training/sft.json'
+    launch_code = 'poetry run python -m turbo_alignment train_sft --experiment_settings_path training/sft.json'
     subprocess.run(launch_code, shell=True)
