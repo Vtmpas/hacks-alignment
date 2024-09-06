22074db0d524351280393881a5fa768cba41f782
Kirill Khvoshchev <kkhvoshchev@ozon.ru>
2024-09-06 18:37:46 +0300

    add schemas

---
+++ app/schemas.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from pydantic import BaseModel
+
+
+class ValidationError(BaseModel):
+    loc: List[str]
+    msg: str
+    type: str
+
+class RequestModel(BaseModel):
+    query: str
+
+class ResponseModel(BaseModel):
+    text: str
--- app/server.py
+++ app/server.py
@@ -1,33 +1,24 @@
 # server.py
 import litserve as ls
+from typing import Union
+from app.schemas import ValidationError, ResponseModel
 
 
-# (STEP 1) - DEFINE THE API (compound AI system)
 class SimpleLitAPI(ls.LitAPI):
     def setup(self, device):
-        # setup is called once at startup. Build a compound AI system (1+ models), connect DBs, load data, etc...
-        self.model1 = lambda x: x**2
-        self.model2 = lambda x: x**3
+        self.llm = lambda x: ResponseModel(text="dummy")
 
-    def decode_request(self, request):
-        # Convert the request payload to model input.
+    def decode_request(self, request, **kwargs):
         return request["input"]
 
-    def predict(self, x):
-        # Easily build compound systems. Run inference and return the output.
-        squared = self.model1(x)
-        cubed = self.model2(x)
-        output = squared + cubed
-        return {"output": output}
+    def predict(self, x, **kwargs) -> Union[ResponseModel, ValidationError]:
+        return self.llm(x)
 
-    def encode_response(self, output):
-        # Convert the model output to a response payload.
+    def encode_response(self, output, **kwargs):
         return {"output": output}
 
 
-# (STEP 2) - START THE SERVER
 if __name__ == "__main__":
-    # scale with advanced features (batching, GPUs, etc...)
     ls.LitServer(
         lit_api=SimpleLitAPI(),
         accelerator="auto",
