2cd84148de4c0d6ca6fd53311633b163c1c7cf54
Kirill Khvoshchev <kkhvoshchev@ozon.ru>
2024-09-07 14:28:02 +0300

    add vllm inference

--- app/server.py
+++ app/server.py
@@ -1,20 +1,40 @@
 # server.py
-import time
+from typing import cast
 
 import litserve as ls
-from schemas import DummyModelOutput, PredictOutputModel, RequestModel, ResponseModel
+from huggingface_hub import snapshot_download
+from schemas import PredictOutputModel, RequestModel, ResponseModel
+from vllm import LLM, SamplingParams
+from vllm.lora.request import LoRARequest
 
 
 class SimpleLitAPI(ls.LitAPI):
+    sampling_params: SamplingParams
+    lora: LoRARequest
+    llm: LLM
+
     def setup(self, device):
-        self.model = lambda x: DummyModelOutput
+        self.lora = LoRARequest(
+            lora_name="sft",
+            lora_int_id=1,
+            lora_path=snapshot_download(repo_id="yard1/llama-2-7b-sql-lora-test"),
+        )
+        self.sampling_params = SamplingParams(
+            temperature=0,
+            max_tokens=256,
+        )
+        self.llm = LLM(model="AnatoliiPotapov/T-lite-instruct-0.1", enable_lora=True)
 
     def decode_request(self, request: RequestModel, **kwargs) -> str:
         return request.query
 
-    def predict(self, x: str, **kwargs) -> PredictOutputModel:
-        time.sleep(1)
-        return self.model(x)
+    def predict(self, prompt: str, **kwargs) -> PredictOutputModel:
+        response = self.llm.generate(
+            prompts=[prompt],
+            sampling_params=self.sampling_params,
+        )
+
+        return cast(PredictOutputModel, response)
 
     def encode_response(self, output: PredictOutputModel, **kwargs) -> ResponseModel:
         return ResponseModel(text=str(output))
@@ -25,6 +45,6 @@ if __name__ == "__main__":
         lit_api=SimpleLitAPI(),
         accelerator="auto",
         max_batch_size=1,
-        api_path="/predict",
+        api_path="/assist",
         stream=False,
     ).run(port=8000)
